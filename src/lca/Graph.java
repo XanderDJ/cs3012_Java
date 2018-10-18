8ad85cb9828e9c0a4450bc8c3927a8aacf0b534b
xanderdj <x.de.j@hotmail.com>
2018-10-18 20:34:44 +0100

    made intial structure for phase 1 of lca

--- src/lca/Graph.java
+++ src/lca/Graph.java
@@ -4,6 +4,7 @@ package lca;
 import util.BTNode;
 import util.Node;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Graph {
@@ -24,7 +25,52 @@ public class Graph {
     }
 
 
+    /**
+     *      HashSet<HashSet<Node>> foundPaths = new HashSet<>();
+     *         if(node1 == node2){
+     *             return node1;
+     *         }
+     *         Node lastNodeFound = node1;
+     *         foundNodes.add(lastNodeFound);
+     *         while(lastNodeFound != getRoot()){
+     *             lastNodeFound = lastNodeFound.getParent();
+     *             foundNodes.add(lastNodeFound);
+     *         }
+     *
+     *         Node lowestCommonAncestor = node2;
+     *         boolean LCAFound = foundNodes.contains(lowestCommonAncestor);
+     *         while (!LCAFound){
+     *             lowestCommonAncestor = lowestCommonAncestor.getParent();
+     *             if(foundNodes.contains(lowestCommonAncestor)){
+     *                 LCAFound = true;
+     *             }
+     *         }
+     *         return lowestCommonAncestor;
+     * @param node1
+     * @param node2
+     * @return
+     */
     public Node lca(Node node1, Node node2) {
+        if(node1 ==node2){
+            return node1;
+        }
+        HashSet<ArrayList<Node>> pathsFound = new HashSet<>();
+        ArrayList<Node> pathInitializer = new ArrayList<>();
+        pathInitializer.add(node1);
+        pathsFound.add(pathInitializer);
+        while(checkIfAllPathsFoundRoot(pathsFound)){
+
+        }
         return null;
+
+    }
+
+    public boolean checkIfAllPathsFoundRoot(HashSet<ArrayList<Node>> paths){
+        for (ArrayList<Node> path:paths) {
+            if(!path.contains(getRoot())){
+                return false;
+            }
+        }
+        return  true;
     }
 }
